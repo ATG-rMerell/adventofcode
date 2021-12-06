1918c408461b6b2134eb48f270b14cc0b0335f6f
Robin Merell <robin.merell@atg.se>
2021-12-06 15:22:30 +0100

    star 2 day 6

--- src/main/kotlin/day6.kt
+++ src/main/kotlin/day6.kt
@@ -6,26 +6,24 @@ fun main() {
         .map { row -> row.split(",").map { it.toInt() } }
         .first()
 
-    println(star1(input, 0))
+    println(calculate(input, 80))
+    println(calculate(input, 256))
 }
 
-fun star1(input: List<Int>, day: Int): Int {
-    if (day == 80) {
-        return input.size
+fun calculate(input: List<Int>, days: Int): Long {
+    var eachCount = input.groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+
+    for (i in 1..days) {
+        eachCount = eachCount.keys.flatMap {
+            if(it == 0) {
+                listOf(6 to eachCount[it], 8 to eachCount[it])
+            } else {
+                listOf((it -1) to eachCount[it])
+            }
+        }.groupBy { it.first }
+            .mapValues {
+                it.value.mapNotNull { it.second  }.sum()
+            }
     }
-
-    val temp = mutableListOf<Int>()
-    temp.addAll(input)
-
-    for ((i, v) in input.withIndex()) {
-        if (v == 0) {
-            temp.add(8)
-            temp[i] = 6
-        } else {
-            temp[i] = temp[i].minus(1)
-        }
-    }
-
-    return star1(temp, day + 1)
+    return eachCount.values.sum()
 }
-
